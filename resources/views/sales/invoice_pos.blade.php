5dc850c2df0d9fbfb23adde7aeb386f2b245fbdc
Bahrom Parpiyev <bahrom1982@gmail.com>
2024-01-10 11:44:26 +0500

    commit

--- resources/views/sales/invoice_pos.blade.php
+++ resources/views/sales/invoice_pos.blade.php
@@ -68,7 +68,7 @@ $setting = DB::table('settings')->where('deleted_at', '=', null)->first();
                   @{{detail_invoice.imei_number}}</span>
               </td>
               <td class="product_detail_invoice">
-                  @{{formatNumber(detail_invoice.quantity,0)}}
+                  @{{detail_invoice.quantity.total}}
               </td>
             </tr>
 
