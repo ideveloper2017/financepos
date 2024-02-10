3c5050f4e5376042a0ebd1f76457c18a483d09b3
Bahrom Parpiyev <bahrom1982@gmail.com>
2024-02-11 03:32:45 +0500

    Change code clients data

--- app/Telegram/MyBotHandler.php
+++ app/Telegram/MyBotHandler.php
@@ -28,9 +28,7 @@ class MyBotHandler extends WebhookHandler
 
         $bot->registerCommands([
             'start'=>'Start bot',
-            'hello' => 'говорит привет',
             'actions' => 'различные действия',
-            'help' => 'что умеет этот бот'
         ])->send();
     }
 
