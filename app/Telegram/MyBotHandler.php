b25ea20a49098752bc0f1a6119b191211923eefb
Bahrom Parpiyev <bahrom1982@gmail.com>
2024-02-12 02:23:38 +0500

    Change code clients data

--- app/Telegram/MyBotHandler.php
+++ app/Telegram/MyBotHandler.php
@@ -68,7 +68,7 @@ class MyBotHandler extends WebhookHandler
         }
         report($throwable);
 
-        $this->reply('sorry man, I failed'.' '.$throwable->getLine().' '.$throwable->getFile().' '.$throwable->getMessage());
+        $this->reply('sorry man, I failed'.' '.$throwable->getLine().' '.$throwable->getMessage());
     }
 
     public function products(Stringable $text)
