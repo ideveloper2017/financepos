56628f4a360dfdd2abe43bb4950acd127fc865a2
Bahrom Parpiyev <bahrom1982@gmail.com>
2024-02-14 03:32:20 +0500

    Change code clients data

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -34,8 +34,6 @@ class TelegramBotController extends Controller
         switch ($this->text){
             case '/start':
                 $content = ['chat_id' => $chat_id, 'text' => 'Welcome to Test GameBot !'];
-                $menu = [["Inline"],["Google News"],["button 1","button 2"]];
-//                $this->telegram->setChatMenuButton($menu);
                 $this->telegram->sendMessage($content);
                 break;
             case '/where':
